0779f9c1d3ee62eec47a2967498afea802d433b2
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2021-01-19 09:07:44 -0800

    add onpreboootstrap

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -1,6 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const withDefaults = require('./utils/default-options');
+const mkdirp = require('mkdirp');
+
 exports.onPreBootstrap = ({ store }, options) => {
   const { program } = store.getState();
-  // TODO options with defaults
+  //path defaults
+  const { contentPath } = withDefaults(options);
+  // dir path
+  const dir = path.join(program.directory, contentPath);
+
+  if (!fs.existsSync(dir)) {
+    //  create the dir
+    mkdirp.sync(dir);
+  }
   // figure out content path
   // if directory doesn't exist create it
 };
